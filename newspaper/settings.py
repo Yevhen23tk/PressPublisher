820b1f17bc5b87dffc33490618593f17f26a3dff
Yevhen Tkachuk <yevhen23tk@gmail.com>
2024-12-21 21:47:38 +0200

    add sample, hide the secret key, update requirements

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-f%&=)vdf+&co&o#(wj)91n&tt=_p(*2o415o2q3!pyf!r0#y+3"
-)
+# SECRET_KEY = (
+#     "django-insecure-f%&=)vdf+&co&o#(wj)91n&tt=_p(*2o415o2q3!pyf!r0#y+3"
+# )
+env_path = BASE_DIR / ".env"
+load_dotenv(dotenv_path=env_path)
 
+SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-fallback-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
